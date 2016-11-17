7f0b22f1cf3f138bc50d2c867037e7fdeefa405c
aobin <ao.bin@hp.com>
2016-11-17 14:08:06 +0800

    进行样式修改

--- demo/js/components/FeedItems.js
+++ demo/js/components/FeedItems.js
@@ -18,19 +18,28 @@ export class FeedItems extends Component
   render()
   {
     let {feedDocument} = this.props;
+
     let items = feedDocument.data.map(
       function (item, index) {
         return (
-          <li key={index}>{item.title}</li>
+          <section className="rss-section show-inline">
+            <img className="rss-section-img" src="images/img_1.png"/>
+            <div className="rss-section-content">
+              <h3>{item.title}</h3>
+              <span><a href="#">265</a> Likes</span>
+              <span><a href="#"> 84</a> Comments</span>
+            </div>
+          </section>
         );
       }
     );
 
     return (
-      <div id="project-comments" className="commentList">
-        <ul>
+      <div id="project-comments" className="rss-block">
+        <h2 className="rss-title">My Inspirations</h2>
+
           {items}
-        </ul>
+
       </div>
     );
   }
